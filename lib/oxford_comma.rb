1bf2bbf628b61c117512399f9b0eb65ef9ed68a9
Adam Clayman <adam@clayman.org>
2017-10-16 04:20:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,10 @@ require 'pry'
 def oxford_comma(array)
   if array.size == 2
     return array.join(" and ")
-  elsif array.size > 2    
+  elsif array.size > 2
     last_element = array.pop
     sentence = array.join(", ")
-    sentence = sentence + "and #{last_element}" 
+    sentence = sentence + "and #{last_element}"
     return sentence
+  end
 end
