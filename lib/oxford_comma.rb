53a409dcd887ee03960ba4fb56ebdcfa842a4ca1
Adam Clayman <adam@clayman.org>
2017-10-16 04:22:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   if array.size == 2
     return array.join(" and ")
   elsif array.size == 1
-    return array
+    return array.join
   elsif array.size > 2
     last_element = array.pop
     sentence = array.join(", ")
